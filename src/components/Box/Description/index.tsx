59601b1e066d995134e4f4766db1a5b55c50d73e
Alessandro <alessandro.barreto@farfetch.com>
2024-02-25 11:56:24 +0000

    feature: box description

---
+++ src/components/Box/Description/index.tsx
@@ -0,0 +1,51 @@
+import { Network } from "../../../api";
+import useNetwork from "../../../hooks/useNetwork";
+import { Layer } from "..";
+
+interface NetworkDescriptionProps {
+  network: Network;
+  currentLayer: Layer;
+}
+
+const NetworkDescription = ({
+  network,
+  currentLayer,
+}: NetworkDescriptionProps) => {
+  const {
+    handlers: { newtorksPerCountry },
+  } = useNetwork();
+
+  if (currentLayer === Layer.L1) {
+    return (
+      <>
+        <h4>{`Name: ${network.name}`}</h4>
+        <h4>{`Country: ${network.location.country}`}</h4>
+        <h4>{`City: ${network.location.city}`}</h4>
+      </>
+    );
+  }
+
+  if (currentLayer === Layer.L2) {
+    return (
+      <>
+        <h4>{`Stations(${network.company.length})`}</h4>
+        {network.company.map((station, index) => (
+          <li key={index}>{station}</li>
+        ))}
+      </>
+    );
+  }
+
+  if (currentLayer === Layer.L3) {
+    return (
+      <>
+        <h4>{`Country: ${network.location.country}`}</h4>
+        <h4>{`Networks : ${newtorksPerCountry(currentLayer)}`}</h4>
+      </>
+    );
+  }
+
+  return null;
+};
+
+export default NetworkDescription;
--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { Network } from "../../api";
-import useNetwork from "../../hooks/useNetwork";
 import "./styles.css";
+import NetworkDescription from "./Description";
 
 interface BoxProps {
   network: Network;
 }
 
-enum Layer {
+export enum Layer {
   L1 = "L1",
   L2 = "L2",
   L3 = "L3",
@@ -16,46 +16,8 @@ enum Layer {
 const Box = ({ network }: BoxProps) => {
   const [currentLayer, setCurrentLayer] = useState(Layer.L1);
 
-  const {
-    handlers: { newtorksPerCountry },
-  } = useNetwork();
-
   const handleClick = (layer: Layer) => {
     setCurrentLayer(layer);
-  };
-
-  const networkDescription = (currentLayer: Layer) => {
-    if (currentLayer === Layer.L1) {
-      return (
-        <>
-          <h4>{`Name: ${network.name}`}</h4>
-          <h4>{`Country: ${network.location.country}`}</h4>
-          <h4>{`City: ${network.location.city}`}</h4>
-        </>
-      );
-    }
-
-    if (currentLayer === Layer.L2) {
-      return (
-        <>
-          <h4>{`Stations(${network.company.length})`}</h4>
-          {network.company.map((station) => (
-            <li>{station}</li>
-          ))}
-        </>
-      );
-    }
-
-    if (currentLayer === Layer.L3) {
-      return (
-        <>
-          <h4>{`Country: ${network.location.country}`}</h4>
-          <h4>{`Total Networks : ${newtorksPerCountry(
-            network.location.country
-          )}`}</h4>
-        </>
-      );
-    }
   };
 
   return (
@@ -71,7 +33,9 @@ const Box = ({ network }: BoxProps) => {
           </div>
         ))}
       </div>
-      <div className="right-side">{networkDescription(currentLayer)}</div>
+      <div className="right-side">
+        <NetworkDescription network={network} currentLayer={currentLayer} />
+      </div>
     </div>
   );
 };
